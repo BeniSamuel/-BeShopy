b5a0878850cdb778e1c0a53ea9084f89249f37aa
Beni Samuel <benisamuel566@gmail.com>
2024-10-08 17:16:30 +0200

    added colors

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=" bg-white flex justify-between px-20 items-center h-20">
-      <div className=" font-bold text-xl text-black">BeShopy</div>
+    <div className=" bg-[#C5F5D6] flex justify-between px-20 items-center h-20">
+      <div className=" font-bold text-xl text-[#224F34]">BeShopy</div>
 
       <ul className=" flex gap-9">
-        <li className=" font-medium text-black">HOME</li>
-        <li className=" font-medium text-black">SHOP</li>
-        <li className=" font-medium text-black">FEATURES</li>
-        <li className=" font-medium text-black">CONTACT</li>
+        <li className=" font-medium text-[#224F34]">HOME</li>
+        <li className=" font-medium text-[#224F34]">SHOP</li>
+        <li className=" font-medium text-[#224F34]">FEATURES</li>
+        <li className=" font-medium text-[#224F34]">CONTACT</li>
       </ul>
 
       <div>
