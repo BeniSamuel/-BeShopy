151578d94d2eb98aaf4be19614e410fdc7e71ea4
Beni Samuel <benisamuel566@gmail.com>
2024-10-17 22:15:33 +0200

    Fixed issues

--- src/Components/Home/BestSelling/BestLast.jsx
+++ src/Components/Home/BestSelling/BestLast.jsx
@@ -5,7 +5,6 @@ const BestLast = () => {
     <div className=' flex flex-row justify-center'>
         <button className=' border-2 border-[#224F34] px-4 py-1'>
             <p className=' font-poppins'>See all</p>
-            <p>See all</p>
             <img/>
         </button>
     </div>
