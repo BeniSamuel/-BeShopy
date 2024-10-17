7f08b8cf51b57b53f95073b151d076cd6a4cb755
Beni Samuel <benisamuel566@gmail.com>
2024-10-17 11:25:26 +0200

    IMproved Sections

--- src/Components/Home/BestSelling/BestSelling.jsx
+++ src/Components/Home/BestSelling/BestSelling.jsx
@@ -4,9 +4,9 @@ import BestLast from './BestLast'
 
 const BestSelling = () => {
   return (
-    <div className=' pt-12 flex flex-col gap-9'>
+    <div className=' py-12 flex flex-col gap-9'>
         <div className=' flex flex-col justify-center items-center'>
-            <div className=' text-[#224F34] font-bold text-xl'>Best Selling</div>
+            <div className=' text-[#224F34] font-bold text-xl font-[#Roboto Slab]'>Best Selling</div>
             <p className=' text-[#224F34] text-sm font-medium'>Get in on the trend with our curated selection of best-selling styles.</p>
         </div>
         {/* // Product Division */}
