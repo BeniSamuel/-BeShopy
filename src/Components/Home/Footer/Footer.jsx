a6320b7e53ad2f24123cc7ccd1199f428514cbb8
Beni Samuel <benisamuel566@gmail.com>
2024-10-18 17:12:58 +0200

    Final Commit

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import twitter from '../../../assets/Twitter.svg'
 const Footer = () => {
   return (
     <div className=' py-14 bg-[#224F34] flex flex-col gap-16'>
-        <div className=' flex flex-col md:flex-row md:gap-40 md:justify-center '>
+        <div className=' flex flex-col gap-8 md:flex-row md:gap-40 md:justify-center '>
             <div className=' flex flex-col gap-3'>
                 <div className=' text-2xl text-white font-bold font-rufina'>BeShopy</div>
                 <p className=' font-roboto text-white'>Social Media</p>
@@ -37,14 +37,14 @@ const Footer = () => {
             <div className=' flex flex-col gap-3'>
                 <div className=' text-white font-roboto'>STAY UP TO DATE</div>
                 <div className=' flex flex-row items-center'>
-                    <input type="email" placeholder='Enter Your Email' className=' bg-transparent placeholder:font-poppins placeholder:text-white border-2 border-[#A3F3BE] py-2 pl-4 outline-none text-white'/>
+                    <input type="email" placeholder='Enter Your Email' className=' bg-transparent placeholder:font-poppins placeholder:text-white border-2 border-[#A3F3BE] px-4 py-2 pl-4 outline-none text-white'/>
                     <input type="submit" value="SUBMIT" className=' font-poppins text-[#224F34] bg-[#A3F3BE] px-4 py-[0.64rem]'/>
                 </div>
             </div>
         </div>
 
         <div className=' flex flex-row items-center gap-4 justify-center'>
-            <div className=' bg-[#A3F3BE] h-[0.12rem] w-[52rem]'></div>
+            <div className=' bg-[#A3F3BE] h-[0.12rem] w-[27rem] md:w-[52rem]'></div>
             <div className=' flex flex-row gap-4'>
                 <p className=' font-poppins text-white text-sm'>Terms</p>
                 <p className=' font-poppins text-white text-sm'>Privacy</p>
